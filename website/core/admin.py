1f5aae66c5157b34e3064239ecdd752819802a03
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2024-02-24 13:38:25 +0300

    Initial commit

---
+++ website/core/admin.py
@@ -0,0 +1,128 @@
+from django.contrib import admin
+from django.db.models import Prefetch
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_login', 'username', 'email', 'phone', 'is_staff', 'mailing_list')
+    list_display_links = ('id', 'last_login', 'username', 'email', 'phone',)
+    search_fields = ('id', 'username', 'phone', 'email')
+    list_filter = ('is_staff',)
+    list_editable = ('is_staff', 'mailing_list')
+
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'manufacturer_name', 'country', 'get_image')
+    list_display_links = ('manufacturer_name',)
+    search_fields = ('manufacturer_name', 'country')
+    list_filter = ('country',)
+    prepopulated_fields = {"slug": ("manufacturer_name",)}
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo.url} width="60" height="60"')
+
+    get_image.short_description = "Логотип"
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_image')
+    readonly_fields = ('get_image',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('name',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.category_photo.url} width="60" height="60"')
+
+    get_image.short_description = "Изображение категории"
+
+
+
+class Product_ImagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'img_name', 'first_img', 'get_image')
+    readonly_fields = ('get_image',)
+    list_display_links = ('img_name',)
+    prepopulated_fields = {'slug': ('img_name',)}
+    search_fields = ('img_name',)
+    list_editable = ('first_img',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.img.url} width="60" height="60"')
+
+    get_image.short_description = "Изображение продукта"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'product_name', 'first_price', 'last_price',  'discount', 'category', 'manufacturer')
+    list_display_links = ('id', 'product_name')
+    search_fields = ('product_name',)
+    list_filter = ('discount', 'category', 'manufacturer', 'last_price',)
+    list_editable = ('first_price', 'last_price', 'discount')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related(
+            'category',
+            'manufacturer',
+            'product_photos')
+        return queryset
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'text', 'rating', 'date')
+    list_filter = ('rating', 'product')
+    search_fields = ('user', 'product')
+
+
+class EmailsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email')
+    list_display_links = ('id', 'email')
+
+
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'characteristic_name', 'value')
+    list_display_links = ('id', 'characteristic_name')
+
+
+class Liked_ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'date')
+    prepopulated_fields = {'slug': ('user', 'product')}
+
+
+class Order_PointsAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'product', 'amount', 'price' , 'in_orders')
+    list_display_links = ('product', 'user')
+    list_filter = ('product', 'user')
+    search_fields = ('product', 'user')
+    list_editable = ('in_orders',)
+
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user',  'get_order_points',  'status', 'price', )
+    list_display_links = ('id', 'user', )
+    search_fields = ('id', 'user')
+    list_filter = ( 'price', 'date')
+    list_editable = ('status', )
+
+    @admin.display(description='пункты заказа')
+    def get_order_points(self, obj):
+        return [order_point.pk for order_point in obj.order_points.all()]
+
+
+admin.site.register(Order_Points, Order_PointsAdmin)
+admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Users, UserAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product_Images, Product_ImagesAdmin)
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Characteristic, CharacteristicAdmin)
+admin.site.register(Liked_Product, Liked_ProductAdmin)
---
+++ website/core/models.py
@@ -0,0 +1,193 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from sortedm2m.fields import SortedManyToManyField
+from autoslug import AutoSlugField
+
+
+class Users(AbstractUser):
+    phone = models.CharField(max_length=20, verbose_name="Телефон", blank=True)
+    slug = AutoSlugField(populate_from='username', unique=True, db_index=True, verbose_name='URL', )
+    mailing_list = models.BooleanField(default=False, blank=True, verbose_name='Рассылка')
+    address = models.CharField(max_length=150, blank=True, verbose_name='Адрес', null=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Characteristic(models.Model):
+    characteristic_name = models.CharField(max_length=30, verbose_name='Название характеристки')
+    value = models.CharField(max_length=20, verbose_name='Значение')
+
+    def __str__(self):
+        return f"{self.characteristic_name} {self.value}"
+
+    class Meta:
+        verbose_name = 'Характеристика товара'
+        verbose_name_plural = 'Характеристи товаров'
+
+
+
+class Manufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=100, verbose_name="Наименование производителя", unique=True)
+    country = models.CharField(max_length=40, verbose_name='Страна', blank=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+    photo = models.ImageField(upload_to='img_category/%Y/%m/%d/', verbose_name='Фотография',
+                              default='shop/static/img/favicon.svg')
+
+    def __str__(self):
+        return self.manufacturer_name
+
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, db_index=True, verbose_name="Название категории", )
+    description = models.TextField(verbose_name="Описание", blank=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+    category_photo = models.ImageField(upload_to='img_category/%Y/%m/%d/', verbose_name='Изображение категории')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория продукта'
+        verbose_name_plural = 'Категории продуктов'
+
+
+
+class Product_Images(models.Model):
+    img_name = models.CharField(max_length=50, verbose_name='Название', blank=True)
+    img = models.ImageField(upload_to='img_product/%Y/%m/%d/', verbose_name='Изображение продукта')
+    first_img = models.BooleanField(default=False, verbose_name='Главная картинка')
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.img_name
+
+    def save(self, *args, **kwargs):
+        if self.first_img:
+            self.img_name = f'{self.img_name}-main'
+            self.slug = f'{self.slug}-main'
+        super(Product_Images, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Фотография продукта'
+        verbose_name_plural = 'Фотографии продуктов'
+
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=70, verbose_name='Название продукта', unique=True, blank=True,
+                                    db_index=True)
+    first_price = models.IntegerField(verbose_name='Первоначальная цена')
+    discount = models.FloatField(verbose_name='Скидка', default=0, blank=True)
+    last_price = models.IntegerField(verbose_name='Конечная  цена', blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=70, unique=True, db_index=True, verbose_name='URL', )
+    description = models.TextField(verbose_name="О продукте", blank=True)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.PROTECT, null=True, verbose_name='Производитель',
+                                     related_name='manufacturer')
+    product_photos = SortedManyToManyField(Product_Images, verbose_name='Изображения', related_name='images',
+                                           blank=True, )
+    product_characteristic = SortedManyToManyField(Characteristic, verbose_name='Характеристики',
+                                                   related_name='characteristics', blank=True, )
+
+    def __str__(self):
+        return self.product_name
+
+
+    def save(self, *args, **kwargs):
+        if self.discount > 0:
+            self.last_price = int(self.first_price * (1 - self.discount / 100))
+        else:
+            self.last_price = self.first_price
+        super(Products, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукция'
+        ordering = ['product_name']
+
+
+class Comments(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey('Products', on_delete=models.CASCADE, verbose_name="Продукты", related_name='comments')
+    text = models.TextField(verbose_name="Комментарий", blank=True)
+    rating = models.IntegerField(verbose_name='Оценка от 1 до 10 ')
+    date = models.DateField(verbose_name='Время', auto_now_add=True, blank=True)
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['rating']
+
+
+
+class Liked_Product(models.Model):
+    user = models.ForeignKey("Users", on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey("Products", on_delete=models.CASCADE, verbose_name="Продукт")
+    slug = models.SlugField(verbose_name="URL", unique=True, blank=True)
+    date = models.DateField(auto_now_add=True, verbose_name="Дата")
+
+    def __str__(self):
+        return f"{self.user}-{self.product}-{self.date}"
+
+    def save(self, *args, **kwargs):
+
+        self.slug = f"{self.user} {self.product}"
+        super(Liked_Product, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Понравившийся продукты'
+        verbose_name_plural = 'Понравившиеся продукты'
+
+
+class Order_Points(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.PROTECT, verbose_name='Пользователь', related_name='user')
+    product = models.ForeignKey('Products', on_delete=models.PROTECT, verbose_name='Продукт', related_name='product')
+
+    amount = models.IntegerField(verbose_name='Количество', default=1)
+    price = models.IntegerField(verbose_name='Цена', blank=True)
+    in_orders = models.BooleanField(verbose_name='Участвует в заказе ', default=False, blank=True)
+
+    def __str__(self):
+        return f"пользователель {self.user} продукт {self.product}"
+
+    def save(self, *args, **kwargs):
+        if not self.in_orders:
+            amount = self.amount
+            product = Products.objects.get(product_name=self.product)
+            product_price = float(product.last_price)
+            self.price = product_price * amount
+        super(Order_Points, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Пункт заказа"
+        verbose_name_plural = "Пункты заказов"
+
+
+
+
+class Orders(models.Model):
+    status_field = (
+        ("Не оплачено", "Не оплачено"),
+        ("Оплачено", "Оплачено"),
+
+    )
+    user = models.ForeignKey('Users', on_delete=models.PROTECT, verbose_name='Пользователь')
+    order_points = SortedManyToManyField(Order_Points, verbose_name="Пункт заказа")
+    price = models.IntegerField(blank=True, verbose_name='Цена заказа')
+    status = models.CharField(max_length=50, verbose_name='Статус', default='Не оплачено', choices=status_field )
+    date = models.DateTimeField(auto_now_add=True, verbose_name="Время заказа")
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f"пользователь-{self.user} заказ-{self.id}"
